f131a0b1903cd8f7352c98e98e1cc138ce67b4c2
AungKaung <aungkaungmyathtun@gmail.com>
2024-05-07 10:32:10 -0700

    Remove

--- NodeJs-day1/practise1.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const file = fs.readFileSync('txt/input.txt','utf-8');
-
-const output = fs.writeFileSync('txt/output.txt','Extra option');
-console.log(file);
-// console.log(output);
--- NodeJs-day1/txt/testing/synchro.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-const file = fs.readFileSync('../input.txt','utf-8');
-console.log("Date read from the file",file);
-
-fs.writeFileSync('example.txt',file);
-console.log("Date write successfully");
